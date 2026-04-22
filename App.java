943ab1ca457a6d8e731c6ae766927b4f4724c3c7
Gathje <adarsh4arun@gmail.com>
2026-04-22 20:40:03 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        int n = 10;
+        if (n % 2 == 0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+}
